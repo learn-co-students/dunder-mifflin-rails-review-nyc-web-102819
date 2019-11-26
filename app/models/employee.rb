0ae501b924db6813e8ec08b7fc59b158796c9333
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-25 23:03:50 -0500

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :alias, :title, uniqueness: true
+  validates :first_name, :last_name, :alias,
+            :title, :office, :dog_id, presence: true
 end
